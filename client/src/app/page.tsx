41d18278698b69b8743720d6a4229efb66dcc1a2
David Umoru <umorudavido@gmail.com>
2024-05-15 20:19:52 +0100

    Change metadata and remove article cards

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -18,8 +18,6 @@ export default function Home() {
             }}
           />
         </div>
-
-        <ThirdwebResources />
       </div>
     </main>
   );
@@ -41,59 +39,19 @@ function Header() {
         EtherSeed
       </h1>
       <h1 className="text-lg md:text-4xl font-medium md:font-semibold tracking-tighter inline-block -skew-x-6 mb-4 text-blue-500">
-        The Future of Crowdfunding...
+        The Future of Crowdfunding!
+      </h1>
+      <h1 className="text-lg md:text-4xl font-medium md:font-semibold tracking-tighter inline-block -skew-x-6 mb-4 text-blue-500">
+        Invest directly in projects using blockchain technology.
       </h1>
 
       <p className="text-zinc-300 text-base">
-        Read the{" "}
+        Click the{" "}
         <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-          README.md
+          Connect
         </code>{" "}
-        file to get started.
+        button to get started.
       </p>
     </header>
-  );
-}
-
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="Getting Started with EtherSeed"
-        href=""
-        description=""
-      />
-
-      <ArticleCard
-        title="EtherSeed Documentation"
-        href=""
-        description=""
-      />
-
-      <ArticleCard
-        title="EtherSeed GitHub Repository"
-        href=""
-        description=""
-      />
-    </div>
-  );
-}
-
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
-  return (
-    <a
-      href={props.href + "?utm_source=next-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
   );
 }
